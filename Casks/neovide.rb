10e8929205d971fb764edd256628f90c89eae3dc
Shanyou Li <shanyouli6@gmail.com>
2024-08-13 16:51:34 +0800

    feat(pkgs): add neovide, upic

---
+++ Casks/neovide.rb
@@ -0,0 +1,23 @@
+cask "neovide" do
+  arch arm: "aarch64", intel: "x86_64"
+
+  version "0.13.3"
+  sha256 arm:   "d17883a1ab1694c24d670986cd4f5856dfede9134cbb4904260e7e76e614dea0",
+         intel: "ce540bc21760972e336b92950e32ad42d579c8db5763ce33c515f877f42ce0b4"
+
+  url "https://github.com/neovide/neovide/releases/download/#{version}/Neovide-#{arch}-apple-darwin.dmg"
+  name "Neovide"
+  desc "Neovim Client"
+  homepage "https://github.com/neovide/neovide"
+
+  # depends_on formula: "neovim"
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+  app "Neovide.app"
+  binary "#{appdir}/Neovide.app/Contents/MacOS/neovide"
+
+  zap trash: "~/Library/Saved Application State/com.neovide.neovide"
+end
+
--- README.md
+++ README.md
@@ -19,3 +19,5 @@ brew "<formula>"
 + [NextChat](https://github.com/ChatGPTNextWeb/ChatGPT-Next-Web): A cross-platform ChatGPT/Gemini UI 
 + [Pearcleaner](https://github.com/alienator88/Pearcleaner): app 卸载工具
 + [LyricsX](https://github.com/MxIris-LyricsX-Project/LyricsX): 歌词显示工具
++ [neovide](https://github.com/neovide/neovide): neovim 的 GUI 工具，将 nvim 和 neovide 分离
++ [uPic](https://github.com/gee1k/uPic): 图床管理工具，最新版需要自己编译
