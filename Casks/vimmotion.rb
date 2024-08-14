dc8d50c520341b3650ca5495e6038c4a3ca6ae96
Shanyou Li <shanyouli6@gmail.com>
2024-08-14 13:42:15 +0800

    feat(pkgs): fix vimmotion install error

--- Casks/vimmotion.rb
+++ Casks/vimmotion.rb
@@ -3,11 +3,13 @@ cask 'vimmotion' do
   sha256 'b7b86c22c9dc16eb75ee7982d5a45ab5c55ab7f14948ece46ec00387a9ce0703'
 
   url "https://github.com/dwarvesf/VimMotionApp/releases/download/v#{version}/VimMotion.#{version.major_minor}.dmg"
-  appcast 'https://github.com/dwarvesf/VimMotionApp/releases.atom'
   name 'VimMotion'
   homepage 'https://github.com/dwarvesf/VimMotionApp/'
 
   depends_on macos: '>= :high_sierra'
-
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
   app 'VimMotion.app'
 end
