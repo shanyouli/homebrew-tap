5ac9d0b0419df5b7431d54ead2f72174ed31689c
Shanyou Li <shanyouli6@gmail.com>
2024-09-30 16:55:34 +0800

    fix spotube

--- Casks/spotube.rb
+++ Casks/spotube.rb
@@ -2,14 +2,13 @@ cask "spotube" do
   version "3.8.0"
   sha256 "a900db1919e26bc2407259b60204f6142c61612e96a0db8358831b1bbea90c1d"
 
-  url "https://github.com/KRTirtho/spotube/releases/download/v#{version}/Spotube-macos-universal.dmg",
-      verified: "github.com/KRTirtho/spotube/"
+  url "https://github.com/KRTirtho/spotube/releases/download/v#{version}/Spotube-macos-universal.dmg"
   name "Spotube"
-  desc "🎧 Open source Spotify client that doesn't require Premium nor uses Electron! Available for both desktop & mobile!"
-  homepage "https://spotube.krtirtho.dev/"
+  desc "🎧 Open source Spotify client"
+  homepage "https://github.com/KRTirtho/spotube"
 
   livecheck do
-    url :url
+    url "https://github.com/KRTirtho/spotube"
     strategy :github_latest
   end
 
