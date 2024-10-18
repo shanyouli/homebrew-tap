7917f688c077bcc07213b12d46419831d4c4317e
Shanyou Li <shanyouli6@gmail.com>
2024-10-18 15:35:18 +0800

    feat(pkgs): add compresso

---
+++ Casks/compresso.rb
@@ -0,0 +1,27 @@
+cask "compresso" do
+  arch arm: "aarch64", intel: "x64"
+
+  version "1.2.0"
+  sha256 arm:   "cc09c2e8bf033b14e5e0279c883af0686333d865a7ee18b6516db3330e30330e",
+         intel: "3b6e2231269355b0ef53035cbd308480f0d6d2d744c325ef4bc137652c39cfd4"
+
+  url "https://github.com/codeforreal1/compressO/releases/download/v#{version}/CompressO_#{version}_#{arch}.dmg"
+  name "compressO"
+  desc "Convert any video into a tiny size"
+  homepage "https://github.com/codeforreal1/compressO"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+
+  app "CompressO.app"
+
+  zap trash: [
+    "~/Library/Application Support/com.compresso.app",
+    "~/Library/Caches/com.compresso.app",
+    "~/Library/Preferences/com.compresso.app.plist",
+    "~/Library/Saved Application State/com.compresso.app.savedState",
+    "~/Library/WebKit/com.compresso.app",
+  ]
+end
--- README.md
+++ README.md
@@ -41,6 +41,7 @@ brew "<formula>"
 + [snapbox](https://snapbox.app/): 依赖 ollama.app 的 ai 本地客户端， 可以使用 api
 + [cozy-reader](https://github.com/srsng/cozy-reader) epub reader, 目前版本0.1.19 无法工作
 + [cherry-studio](https://github.com/kangfenmao/cherry-studio) AI chat 客户端
++ [compressO](https://github.com/codeforreal1/compressO) 视频压缩工具
 
 ## 部分安装脚本来源
 
