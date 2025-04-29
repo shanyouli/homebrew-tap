1c5a21da782fea42dbf5ed8f6dd694a2eeabcf20
Shanyou Li <shanyouli6@gmail.com>
2025-04-29 18:39:45 +0800

    feat: fix mind-map

--- Casks/mind-map.rb
+++ Casks/mind-map.rb
@@ -5,7 +5,7 @@ cask "mind-map" do
   sha256 arm:   "9929d2adb6e4ac6b5381ad2f0a6240b20b35406043f2f5366df29361acef459d",
          intel: "adc2fc820064aae7af12e14ab91a51b7414e06c0bb28e5ff41a336e236b5fc27"
 
-  url "https://github.com/wanglin2/mind-map/releases/download/electron-#{version}/-mac-#{version}-#{arch}.dmg"
+  url "https://github.com/wanglin2/mind-map/releases/download/#{version}/-mac-#{version}-#{arch}.dmg"
   name "mind-map"
   desc "Relatively powerful web mind map"
   homepage "https://github.com/wanglin2/mind-map"
@@ -13,7 +13,7 @@ cask "mind-map" do
   livecheck do
     url "https://github.com/wanglin2/mind-map/releases/latest"
     strategy :page_match
-    regex(%r{href=.*?/tag/electron-(\d+(?:\.\d+)+)["' >]}i)
+    regex(%r{href=.*?/tag/(\d+(?:\.\d+)+)["' >]}i)
   end
 
   app "思绪思维导图.app"
