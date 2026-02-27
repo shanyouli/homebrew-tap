358b919022af6eb821c7d7abf0c2a3d43d559278
Shanyou Li <shanyouli6@gmail.com>
2026-02-27 14:19:19 +0800

    feat: fix warden

--- Casks/warden.rb
+++ Casks/warden.rb
@@ -2,8 +2,8 @@ cask "warden" do
   version "0.9.6"
   sha256 "e466e8300093dca7c990d3c8e6a7af9d159955019585334b0151bbda8cbabc30"
 
-  url "https://github.com/SidhuK/WardenApp/releases/download/v#{version}/Warden.zip",
-      verified: "github.com/SidhuK/WardenApp/"
+  url "https://github.com/SidhuK/WardenApp/releases/download/v#{version}/Warden.zip"
+      # verified: "github.com/SidhuK/WardenApp/"
   name "Warden"
   desc "Native macOS AI chat client supporting 10+ providers"
   homepage "https://github.com/SidhuK/WardenApp"
@@ -14,7 +14,7 @@ cask "warden" do
   end
 
   app "Warden.app"
-
+  depends_on :macos => :sequoia
   zap trash: [
     "~/Library/Application Support/Warden",
     "~/Library/Preferences/com.SidhuK.Warden.plist",
